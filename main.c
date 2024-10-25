4a3727b77059f18a2e96779ac044fb4f3a595889
zap8600 <zapost86@gmail.com>
2024-10-24 21:45:45 -0500

    wip

--- main.c
+++ main.c
@@ -33,218 +33,73 @@
 #define TOK_LE 133
 #define TOK_GE 153
 
-bool tokisnum = false;
-bool tokiscalltrailingparen = false;
+FILE* c;
 
-uint8_t ch;
-uint16_t scratch; // `ax`.
-uint16_t scratchsave;
-uint16_t currenttoken; // `bx`.
-uint16_t currenttokensave; 
-uint16_t lasttwochars; // `cx`.
-uint16_t tablescan; // `si`.
-uint8_t *symboltable; // `ds`.
-uint16_t codegenoffset; // `di`.
-uint16_t codegenoffsetsave;
-uint8_t *codegenbuffer; // `es`.
+int semicolon;
+int ch;
+
+void getch() {
+    if(semicolon) {
+        semicolon = 0;
+        ch = 59;
+    }
+    ch = getc(c); // TODO: Expand subset, as this is incompatible with this subset of C, meaning the compiler can't compile itself.
+    if(ch == 59) {
+        semicolon = 1;
+        ch = 0;
+    }
+}
+
+int token;
+int ch_1;
+int ch_0;
+int tok_is_num;
+int tok_is_call;
+int trailing_parens;
 
 void tok_next() {
-toknext:
-    // TODO: Get character.
-    ch = (uint8_t)(scratch & 0x00ff);
-    if(ch <= 32) {
-        goto toknext;
+    getch();
+    if(ch == 32) {
+        while(ch == 32) {
+            getch();
+        }
     }
 
-    currenttoken = 0;
-    lasttwochars = 0;
+    token = 0;
 
     if(ch <= 57) {
-        tokisnum = true;
+        tok_is_num = 1;
     }
 
-nextch:
-    while(1) {
-        if(ch <= 32) {
-            break;
-        }
+    while(ch != 32) {
+        ch_1 = ch_0;
+        ch_0 = ch;
 
-        lasttwochars = (lasttwochars << 8) | ((uint16_t)scratch & 0xff);
+        token = token * 10;
+        ch = ch - 48;
+        token = token + ch;
 
-        currenttoken = scratch * 10;
-        scratch = scratch - 48;
-        currenttoken = currenttoken + scratch;
-
-        // TODO: Get character.
+        getch();
     }
 
-done:
-    scratch = lasttwochars;
-
-    if(scratch == 0x2f2f) {
-        goto comment_double_slash;
-    } else if (scratch == 0x2f2a) {
-        goto comment_multi_line;
+    ch = ch_0;
+    if(ch == 12079) {
+        //
+    }
+    if(ch == 12074) {
+        /**/
+    }
+    if(ch == 10281) {
+        trailing_parens = 1;
     }
 
-    if(scratch == 0x2829) {
-        tokiscalltrailingparen = true;
-    }
-
-    scratch = currenttoken;
-    return;
-
-comment_double_slash:
-    // TODO: Get character.
-    while(ch != 10) {
-        // TODO: Get character.
-    }
-    goto toknext;
-
-comment_multi_line:
-    tok_next();
-    while(scratch != 65475) {
-        tok_next();
-    }
-    goto toknext;
+    ch = token;
 }
 
-void tok_next2() {
-    tok_next();
-    goto toknext;
-}
-
-void control_block_flow() {
-    compile_stmts_tok_next();
-
-    // TODO: Emit `test ax,ax` instruction.
-    codegenbuffer[codegenoffset] = (uint8_t)(scratch >> 8);
-    codegenbuffer[codegenoffset + 1] = (uint8_t)(scratch & 0x00ff);
-    codegenoffset = codegenoffset + 2;
-    // TODO: Emit jump if equal instruction.
-    codegenbuffer[codegenoffset] = (uint8_t)(scratch >> 8);
-    codegenbuffer[codegenoffset + 1] = (uint8_t)(scratch & 0x00ff);
-    codegenoffset = codegenoffset + 2;
-    codegenbuffer[codegenoffset] = (uint8_t)(scratch >> 8);
-    codegenbuffer[codegenoffset + 1] = (uint8_t)(scratch & 0x00ff);
-    codegenoffset = codegenoffset + 2;
-
-    codegenoffsetsave = codegenoffset;
-    compile_stmts_tok_next();
-    tablescan = codegenoffsetsave;
-}
-
-void compile_assign() {
-    if(scratch != TOK_DEREF) {
-        goto not_deref_store;
-    }
-    tok_next();
-    save_var_and_compile_expr();
-}
-
-void compile_stmts() {
-compilestmts:
-    scratch = currenttoken;
-    if(scratch == TOK_BLK_END) {
-        return;
+int main(int argc, char** argv) {
+    if(argc != 2) {
+        fprintf(stderr, "Usage: %s [C file]\n", argv[0]);
     }
 
-    if(!tokiscalltrailingparen) {
-        goto not_call;
-    }
-
-    // TODO: Emit call instruction to scratch;
-    codegenbuffer[codegenoffset] = (uint8_t)(scratch & 0x00ff);
-    codegenoffset = codegenoffset + 1;
-
-    scratch = symboltable[currenttoken];
-    scratch = scratch - codegenoffset;
-    scratch = scratch - 2;
-    codegenbuffer[codegenoffset] = (uint8_t)(scratch >> 8);
-    codegenbuffer[codegenoffset + 1] = (uint8_t)(scratch & 0x00ff);
-    codegenoffset = codegenoffset + 2;
-
-    goto compilestmtstoknext2;
-
-not_call:
-    if(scratch != TOK_ASM) {
-        goto not_asm;
-    }
-    tok_next();
-    codegenbuffer[codegenoffset] = (uint8_t)(scratch & 0x00ff);
-    codegenoffset = codegenoffset + 1;
-    goto compilestmtstoknext2;
-
-not_asm:
-    if(scratch != TOK_IF_BEGIN) {
-        goto not_if;
-    }
-    control_block_flow();
-    goto patch_fwd;
-
-not_if:
-    if(scratch != TOK_WHILE_BEGIN) {
-        goto not_while;
-    }
-    codegenoffsetsave = codegenoffset;
-    control_block_flow();
-    goto patch_back;
-
-not_while:
-    compile_assign();
-    goto compilestmts;
-
-patch_back:
-    // TODO: Emit jump instruction.
-    codegenbuffer[codegenoffset] = (uint8_t)(scratch & 0x00ff);
-    codegenoffset = codegenoffset + 1;
-    scratch = codegenoffsetsave; // I think this is right.
-    scratch = scratch - codegenoffset;
-    scratch = scratch - 2;
-    codegenbuffer[codegenoffset] = (uint8_t)(scratch >> 8);
-    codegenbuffer[codegenoffset + 1] = (uint8_t)(scratch & 0x00ff);
-    codegenoffset = codegenoffset + 2;
-
-patch_fwd:
-    scratch = codegenoffset;
-    scratch = scratch - tablescan;
-    codegenbuffer[tablescan - 2] = (uint8_t)(scratch >> 8);
-    codegenbuffer[tablescan - 1] = (uint8_t)(scratch & 0x00ff);
-    goto compilestmtstoknext;
-
-compilestmtsend:
-    return;
-}
-
-void compile_stmts_tok_next() {
-compilestmtstoknext:
-    tok_next();
-    goto compilesmts;
-}
-
-void compile_stmts_tok_next2() {
-compilestmtstoknext2:
-    tok_next();
-    goto compilestmtstoknext;
-}
-
-int main() {
-    codegenoffset = 0;
-
-compile:
-    while(1) {
-        tok_next();
-
-        if(scratch != TOK_INT) {
-            break;
-        }
-
-        tok_next2();
-    }
-
-compile_function:
-    tok_next();
-    currenttokensave = currenttoken;
-    symboltable[currenttoken] = codegenoffset;
-    compile_stmts_tok_next2();
+    c = fopen(argv[1], "rb");
 }
