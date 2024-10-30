959e138156eeddf0f8a503c7f039aff757d267d1
zap8600 <95487746+zap8600@users.noreply.github.com>
2024-10-30 14:47:46 -0500

    wip

--- main.c
+++ main.c
@@ -5,15 +5,15 @@
 
 // Some of these values won't work in my compiler, since they used wrapping on 16-bit integers, while I'm using 32-bit integers
 #define TOK_INT 6388
-#define TOK_VOID // 11386 doesn't work here.
+#define TOK_VOID 76922 // 11386 doesn't work here.
 #define TOK_ASM 5631
-#define TOK_COMM 65532
+#define TOK_COMM -4 // 65532 doesn't work here
 #define TOK_SEMI 11
 #define TOK_LPAREN -8 // 65528 doesn't work here.
 #define TOK_RPAREN -7 // 65529 doesn't work here.
 #define TOK_START 544362713 // 20697 doesn't work here.
 #define TOK_DEREF -6161267 // 64653 doesn't work here.
-#define TOK_WHILE_BEGIN 55810
+#define TOK_WHILE_BEGIN 7723522 // 55810 doesn't work here.
 #define TOK_IF_BEGIN 6232
 #define TOK_BODY_BEGIN 5
 #define TOK_BLK_BEGIN 75
@@ -22,6 +22,7 @@
 #define TOK_ADDR -10 // 65526 doesn't work here.
 #define TOK_SUB -3 // 65533 doesn't work here.
 #define TOK_ADD -5 // 65531 doesn't work here.
+#define TOK_DIV -1 // Added by me.
 #define TOK_MUL -6 // 65530 doesn't work here.
 #define TOK_AND -10 // 65526 doesn't work here.
 #define TOK_OR 76
@@ -101,16 +102,49 @@ int compile_unary(const int ttoken) {
         printf("    mov ax,imm ");
     } else {
         printf("    mov ax,[imm] ");
-        token *= 2;
+        token *= 4; // Originally 2 in sectorc, now 4 for 32-bit systems
     }
     printf("%u;\n", token);
     return tok_next();
 }
 
-int functionname;
 int dest;
+int compile_assign(const int ttoken) {
+    int token = ttoken;
+    dest = token;
+    tok_next();
+    token = tok_next();
+    token = compile_unary(token);
+    int op = 0;
+    switch(token) {
+        case TOK_ADD: op = TOK_ADD; break;
+        case TOK_SUB: op = TOK_SUB; break;
+        case TOK_EQ: op = TOK_EQ; break;
+        case TOK_NE: op = TOK_NE; break;
+        default: break; // TODO: Add other operators
+    }
+
+    if(op != 0) {
+        printf("    push ax;\n");
+        token = tok_next();
+        token = compile_unary(token);
+        printf("    pop cx;\n    xchg ax,cx;\n");
+        switch(op) {
+            case TOK_ADD: printf("    add ax,cx;\n");
+        }
+    }
+
+    token = dest;
+    printf("    mov [imm],ax ");
+    token *= 2;
+    printf("%d;\n", token);
+
+    return tok_next();
+}
+
+int functionname;
 int main(int argc, char** argv) {
-    if(argc != 2) {
+    if(argc != 3) {
         fprintf(stderr, "Usage: %s [C file]", argv[0]);
         return -1;
     }
@@ -127,32 +161,7 @@ int main(int argc, char** argv) {
             tok_next();
             token = tok_next();
             while(token != TOK_BLK_END) {
-                dest = token;
-                tok_next();
-                token = tok_next();
-                token = compile_unary(token);
-                int op = 0;
-                switch(token) {
-                    case TOK_ADD: op = TOK_ADD; break;
-                    default: break; // TODO: Add other operators
-                }
-
-                if(op != 0) {
-                    printf("    push ax;\n");
-                    token = tok_next();
-                    token = compile_unary(token);
-                    printf("    pop cx;\n    xchg ax,cx;\n");
-                    switch(op) {
-                        case TOK_ADD: printf("    add ax,cx;\n");
-                    }
-                }
-
-                token = dest;
-                printf("    mov [imm],ax ");
-                token *= 2;
-                printf("%d;\n", token);
-
-                token = tok_next();
+                token = compile_assign(token);
             }
             printf("    return;\n}\n\n");
             token = functionname;
