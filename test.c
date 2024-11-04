245fef8783fc12360fe32ec46750605fdbb2cf42
zap8600 <95487746+zap8600@users.noreply.github.com>
2024-11-04 09:02:05 -0600

    wip

--- test.c
+++ test.c
@@ -1,10 +1,20 @@
+/*
+Makes a good test to see if comments are supported by my tokenizer too.
+tmp1 should have no offset.
+tmp2 should have an offset of 8.
+tmp3 should have an offset of 16.
+tmp4 should have an offset of 24.
+tmp5 should have an offset of 32.
+*/
 int tmp1;
 int tmp2;
 int tmp3;
 int tmp4;
+int tmp5;
 void _start() {
-    tmp1 = 3;
-    tmp2 = 2;
-    tmp3 = tmp1 + 2;
-    tmp4 = tmp2 + tmp1;
+    tmp1 = 1;
+    tmp2 = 4;
+    tmp3 = tmp2 + tmp1;
+    tmp4 = 1 + tmp4;
+    tmp5 = tmp4 - 1;
 }
