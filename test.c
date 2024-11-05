e36ef6c405176abd19373461fd5526bb92abae5d
zap8600 <95487746+zap8600@users.noreply.github.com>
2024-11-05 17:09:01 -0600

    maybe

--- test.c
+++ test.c
@@ -7,10 +7,10 @@ void _start() {
     tmp1 = 3;
     tmp2 = 2;
     tmp3 = tmp1 + tmp2;
-    if( tmp3 == 5 ){
+    if( 5 == 5 ){
         tmp4 = 1;
     }
-    if( tmp4 != 2 ){
+    if( 5 != 2 ){
         tmp5 = 2;
     }
 }
