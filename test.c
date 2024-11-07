435a7661cd45d91bb2df95902ccf239a909ab994
zap8600 <zapost86@gmail.com>
2024-11-06 18:02:20 -0600

    prints "Hi!" and exits

--- test.c
+++ test.c
@@ -1,5 +1,52 @@
-int tmp1;
-int tmp2;
+int syscall_NR;
+int syscall_return;
+int syscall_arg0;
+int syscall_arg1;
+int syscall_arg2;
+int syscall_arg3;
+int syscall_arg4;
+int syscall_arg5;
+
+void syscall() {
+    asm 83;
+    asm 85;
+
+    syscall_arg5 = syscall_arg5;
+    asm 137; asm 197;
+    syscall_arg4 = syscall_arg4;
+    asm 137; asm 199;
+    syscall_arg3 = syscall_arg3;
+    asm 137; asm 198;
+    syscall_arg2 = syscall_arg2;
+    asm 137; asm 194;
+    syscall_arg1 = syscall_arg1;
+    asm 137; asm 193;
+    syscall_arg0 = syscall_arg0;
+    asm 80;
+    syscall_NR = syscall_NR;
+
+    asm 91;
+    asm 205; asm 128;
+
+    asm 93;
+    asm 91;
+
+    asm 129; asm 195; asm 4; asm 0; asm 0; asm 0;
+    asm 137; asm 3;
+    asm 129; asm 235; asm 4; asm 0; asm 0; asm 0;
+}
+
+int msg;
+void main() {
+    msg = 270625096;
+
+    syscall_NR = 4;
+    syscall_arg0 = 1;
+    syscall_arg1 = & msg;
+    syscall_arg2 = 4;
+    syscall();
+}
+
 void _start() {
     asm 187;
     asm 0;
@@ -12,22 +59,11 @@ void _start() {
     asm 229;
     asm 131;
     asm 236;
-    asm 4;
+    asm 16;
 
-    tmp1 = 3;
-    tmp2 = & tmp1;
-    *(int*) tmp2 = *(int*) tmp2 + 2;
+    main();
+
+    syscall_NR = 1;
+    syscall_arg0 = 0;
+    syscall();
 }
-
-    asm 184;
-    asm 1;
-    asm 0;
-    asm 0;
-    asm 0;
-    asm 187;
-    asm 0;
-    asm 0;
-    asm 0;
-    asm 0;
-    asm 205;
-    asm 128;
